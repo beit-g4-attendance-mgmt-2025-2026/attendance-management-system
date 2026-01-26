68f8b308d3a81cd91d24fec299a7ca4d99728405
Myo Thant Naing <thant8642@gmail.com>
2026-01-27 00:53:13 +0630

    :sparkles: feat(take attendance):

--- schema/index.schema.ts
+++ schema/index.schema.ts
@@ -1,150 +1,170 @@
+import { days, months, times } from "@/constants/index.constants";
 import { z } from "zod";
 
 export const LoginSchema = z.object({
-	username: z.string().min(2, {
-		message: "Please enter your username",
-	}),
-	password: z.string().min(6, {
-		message: "Password must be at least 6 characters",
-	}),
+  username: z.string().min(2, {
+    message: "Please enter your username",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters",
+  }),
 });
 
 export const TeacherSchema = z.object({
-	full_name: z.string().min(2, {
-		message: "Name must be at least 2 characters",
-	}),
-	username: z.string().min(4, {
-		message: "Username must be at least 4 characters",
-	}),
-	email: z.string().email({
-		message: "Invalid email address",
-	}),
-	password: z
-		.string()
-		.min(6, { message: "Password must be at least 6 characters" }),
-	phone: z.string().min(6, {
-		message: "Phone number must be at least 6 characters",
-	}),
-	gender: z.enum(["male", "female", "other"], {
-		message: "Gender must be male, female, or other",
-	}),
-	// .refine((val) => val == "Gender", {
-	// 	message:
-	// 		"Auto-detection is not allowed. Please select male, female or other.",
-	// }),
-	role: z.enum(["admin", "department", "teacher"], {
-		message: "Role must be a valid option",
-	}),
-	department: z.enum(["Civil", "CEIT", "EC", "MP", "EP"], {
-		message: "Department must be a valid option",
-	}),
+  full_name: z.string().min(2, {
+    message: "Name must be at least 2 characters",
+  }),
+  username: z.string().min(4, {
+    message: "Username must be at least 4 characters",
+  }),
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
+  phone: z.string().min(6, {
+    message: "Phone number must be at least 6 characters",
+  }),
+  gender: z.enum(["male", "female", "other"], {
+    message: "Gender must be male, female, or other",
+  }),
+  // .refine((val) => val == "Gender", {
+  // 	message:
+  // 		"Auto-detection is not allowed. Please select male, female or other.",
+  // }),
+  role: z.enum(["admin", "department", "teacher"], {
+    message: "Role must be a valid option",
+  }),
+  department: z.enum(["Civil", "CEIT", "EC", "MP", "EP"], {
+    message: "Department must be a valid option",
+  }),
 });
 
 export const StudentSchema = z.object({
-	name: z.string().min(2, {
-		message: "Name must be at least 2 characters",
-	}),
-	phone: z.string().min(6, {
-		message: "Phone number must be at least 6 characters",
-	}),
-	email: z.string().email({
-		message: "Invalid email address",
-	}),
-	gender: z.enum(["male", "female", "other"], {
-		message: "Gender must be male, female, or other",
-	}),
-	department: z.enum(["Civil", "CEIT", "EC", "MP", "EP"], {
-		message: "Please choose department",
-	}),
-	role_no: z.string().min(2, { message: "Please enter role number" }),
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters",
+  }),
+  phone: z.string().min(6, {
+    message: "Phone number must be at least 6 characters",
+  }),
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+  gender: z.enum(["male", "female", "other"], {
+    message: "Gender must be male, female, or other",
+  }),
+  department: z.enum(["Civil", "CEIT", "EC", "MP", "EP"], {
+    message: "Please choose department",
+  }),
+  role_no: z.string().min(2, { message: "Please enter role number" }),
 
-	acedamic_year: z.enum(
-		[
-			"first year",
-			"second year",
-			"third year",
-			"fourth year",
-			"fifth year",
-			"sixth year",
-		],
-		{
-			message: "Please choose acedamic year",
-		}
-	),
-	semester: z.enum(["first semester", "second semester"], {
-		message: "Please choose semester",
-	}),
+  acedamic_year: z.enum(
+    [
+      "first year",
+      "second year",
+      "third year",
+      "fourth year",
+      "fifth year",
+      "sixth year",
+    ],
+    {
+      message: "Please choose acedamic year",
+    }
+  ),
+  semester: z.enum(["first semester", "second semester"], {
+    message: "Please choose semester",
+  }),
 });
 
 export const DepartmentSchema = z.object({
-	name: z.string().min(2, {
-		message: "Please enter department name",
-	}),
-	short_term: z.string().min(2, {
-		message: "Please enter department short term",
-	}),
-	image: z.string().url(),
-	email: z.string().email({
-		message: "Invalid email address",
-	}),
-	phone: z.string().min(6, {
-		message: "Enter phone number",
-	}),
+  name: z.string().min(2, {
+    message: "Please enter department name",
+  }),
+  short_term: z.string().min(2, {
+    message: "Please enter department short term",
+  }),
+  image: z.string().url(),
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+  phone: z.string().min(6, {
+    message: "Enter phone number",
+  }),
 });
 
 export const SubjectSchema = z.object({
-	name: z.string().min(2, {
-		message: "Please enter subject name",
-	}),
-	code: z.string().min(2, {
-		message: "Please enter subject code",
-	}),
-	teacher_name: z.string().min(2, {
-		message: "Please enter username of teacher",
-	}),
-	semester: z.enum(["first semester", "second semester"], {
-		message: "Please choose semester",
-	}),
-	acedamic_year: z.enum(
-		[
-			"first year",
-			"second year",
-			"third year",
-			"fourth year",
-			"fifth year",
-			"sixth year",
-		],
-		{
-			message: "Please choose year",
-		}
-	),
+  name: z.string().min(2, {
+    message: "Please enter subject name",
+  }),
+  code: z.string().min(2, {
+    message: "Please enter subject code",
+  }),
+  teacher_name: z.string().min(2, {
+    message: "Please enter username of teacher",
+  }),
+  semester: z.enum(["first semester", "second semester"], {
+    message: "Please choose semester",
+  }),
+  acedamic_year: z.enum(
+    [
+      "first year",
+      "second year",
+      "third year",
+      "fourth year",
+      "fifth year",
+      "sixth year",
+    ],
+    {
+      message: "Please choose year",
+    }
+  ),
 });
 
 export const ClassSchema = z.object({
-	name: z.string().min(2, {
-		message: "Please enter class name",
-	}),
-	teacher_name: z.string().min(2, {
-		message: "Please enter username of teacher",
-	}),
-	semester: z.enum(["first semester", "second semester"], {
-		message: "Please choose semester",
-	}),
-	acedamic_year: z.enum(
-		[
-			"first year",
-			"second year",
-			"third year",
-			"fourth year",
-			"fifth year",
-			"sixth year",
-		],
-		{
-			message: "Please choose year",
-		}
-	),
+  name: z.string().min(2, {
+    message: "Please enter class name",
+  }),
+  teacher_name: z.string().min(2, {
+    message: "Please enter username of teacher",
+  }),
+  semester: z.enum(["first semester", "second semester"], {
+    message: "Please choose semester",
+  }),
+  acedamic_year: z.enum(
+    [
+      "first year",
+      "second year",
+      "third year",
+      "fourth year",
+      "fifth year",
+      "sixth year",
+    ],
+    {
+      message: "Please choose year",
+    }
+  ),
 });
 
+const dayValues = days.map((d) => d.value) as [string, ...string[]];
+const monthValues = months.map((m) => m.value) as [string, ...string[]];
+const timeValues = times.map((m) => m.value) as [string, ...string[]];
+export const TakeAttendanceSchema = z.object({
+  SubjectId: z.string(),
+  Day: z.enum(dayValues, {
+    message: "Please choose a day",
+  }),
+  Month: z.enum(monthValues, {
+    message: "Please choose a month",
+  }),
+
+  TotalTimes: z.enum(timeValues, {
+    message: "Please choose total time",
+  }),
+  Times: z.record(z.string(), z.enum(timeValues)),
+});
+
+export type TakeAttendanceSchemaType = z.infer<typeof TakeAttendanceSchema>;
 export type LoginSchemaType = z.infer<typeof LoginSchema>;
 export type TeacherSchemaType = z.infer<typeof TeacherSchema>;
 export type StudentSchemaType = z.infer<typeof StudentSchema>;
