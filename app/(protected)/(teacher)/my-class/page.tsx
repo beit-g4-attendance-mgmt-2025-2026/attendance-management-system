d83153ea276b87f82e0dad3834559f074bdf11ab
Thurein Naing <thuureinnnaingg412@gmail.com>
2026-02-01 20:09:39 +0630

    fix:git conflict

--- app/(protected)/(teacher)/my-class/page.tsx
+++ app/(protected)/(teacher)/my-class/page.tsx
@@ -1,6 +1,8 @@
 import ClassCard from "@/components/ClassCard";
 import React from "react";
 import ClassForm from "@/components/ClassForm";
+import SearchInput from "@/components/inputs/SearchInput";
+import { DialogCardBtn } from "@/components/DialogCardBtn";
 
 export interface ClassItem {
 	id: string;
