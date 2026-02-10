f8ab97964b8cb9b88ee2d4431101d2d05f567fc8
Aye Chan Soe <ayechansoe.dev@gmail.com>
2026-02-10 22:50:39 +0630

    Updated Prisma Version 7.2

--- lib/user.ts
+++ lib/user.ts
@@ -1,16 +1,16 @@
-import type { User } from "@/app/generated/prisma/client";
+import type { User } from "@/generated/prisma/client";
 
 export type PublicUser = Omit<
-	User,
-	"password" | "resetPasswordToken" | "resetPasswordExpireAt"
+  User,
+  "password" | "resetPasswordToken" | "resetPasswordExpireAt"
 >;
 
 export function toPublicUser(user: User): PublicUser {
-	const {
-		password: _password,
-		resetPasswordToken: _resetPasswordToken,
-		resetPasswordExpireAt: _resetPasswordExpireAt,
-		...rest
-	} = user;
-	return rest;
+  const {
+    password: _password,
+    resetPasswordToken: _resetPasswordToken,
+    resetPasswordExpireAt: _resetPasswordExpireAt,
+    ...rest
+  } = user;
+  return rest;
 }
