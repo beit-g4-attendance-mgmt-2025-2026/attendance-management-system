19d66791a993e0833b66d69ddebd90c7f5012a16
Thurein Naing <thuureinnnaingg412@gmail.com>
2026-03-28 17:54:00 +0630

    sentry setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,37 @@
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
+
+const securityHeaders = [
+	{
+		key: "Content-Security-Policy",
+		value: `
+      default-src 'self';
+      script-src 'self' 'unsafe-eval' 'unsafe-inline';
+      style-src 'self' 'unsafe-inline';
+      img-src 'self' data: https: http://localhost:3000;
+      font-src 'self';
+      object-src 'none';
+      base-uri 'self';
+      form-action 'self';
+      frame-ancestors 'none';
+    `
+			.replace(/\s{2,}/g, " ")
+			.trim(),
+	},
+];
 
 const nextConfig: NextConfig = {
 	reactStrictMode: false,
+
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: securityHeaders,
+			},
+		];
+	},
+
 	async redirects() {
 		return [
 			{
@@ -23,4 +53,40 @@ const nextConfig: NextConfig = {
 	},
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+	// For all available options, see:
+	// https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+	org: "thurein-naing",
+
+	project: "attendance-management-system",
+
+	// Only print logs for uploading source maps in CI
+	silent: !process.env.CI,
+
+	// For all available options, see:
+	// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+	// Upload a larger set of source maps for prettier stack traces (increases build time)
+	widenClientFileUpload: true,
+
+	// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+	// This can increase your server load as well as your hosting bill.
+	// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+	// side errors will fail.
+	tunnelRoute: "/monitoring",
+
+	webpack: {
+		// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+		// See the following for more information:
+		// https://docs.sentry.io/product/crons/
+		// https://vercel.com/docs/cron-jobs
+		automaticVercelMonitors: true,
+
+		// Tree-shaking options for reducing bundle size
+		treeshake: {
+			// Automatically tree-shake Sentry logger statements to reduce bundle size
+			removeDebugLogging: true,
+		},
+	},
+});
